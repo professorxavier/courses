bba1b0ada1f4b34d28ff79b6c9fa19d226fae602
professorxavier <professorxavier@users.noreply.github.com>
2018-09-17 18:30:56 -0300

    inclusao de inserts de clientes

--- clientserver/tarefas/aula6/sql/base_aula6.sql
+++ clientserver/tarefas/aula6/sql/base_aula6.sql
@@ -5,6 +5,10 @@ use aula6;
 create table cliente (
 	cliente_id int auto_increment,
     nome varchar(200) NOT NULL,
-	limiteVenda decimal DEFAULT 0,
+	limitevenda decimal DEFAULT 0,
 	constraint client_PK primary key (cliente_id)
 );
+
+insert into cliente (nome, limitevenda) VALUES ('Fernando', 100);
+insert into cliente (nome, limitevenda) VALUES ('Neymar', 100000);
+insert into cliente (nome, limitevenda) VALUES ('Tite', 0);
