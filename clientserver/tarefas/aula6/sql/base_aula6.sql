8de6fcafbfb80a5e20bac4971da3f98328956429
professorxavier <professorxavier@users.noreply.github.com>
2018-09-20 17:52:38 -0300

    insert para mais uma tabela

--- clientserver/tarefas/aula6/sql/base_aula6.sql
+++ clientserver/tarefas/aula6/sql/base_aula6.sql
@@ -12,3 +12,13 @@ create table cliente (
 insert into cliente (nome, limitevenda) VALUES ('Fernando', 100);
 insert into cliente (nome, limitevenda) VALUES ('Neymar', 100000);
 insert into cliente (nome, limitevenda) VALUES ('Tite', 0);
+
+create table fornecedor (
+	fornecedor_id int auto_increment,
+    nome varchar(200) NOT NULL,
+	endereco varchar(400) NOT NULL,
+	constraint fornecedor_PK primary key (fornecedor_id)
+);
+
+insert into fornecedor (nome, endereco) VALUES ('Microsoft', 'Rua X - EUA');
+insert into fornecedor (nome, endereco) VALUES ('Google', 'Rua Y - EUA');
