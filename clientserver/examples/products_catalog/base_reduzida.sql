17c1ca25578a5522e6aa24fae53d07c09054db3a
professorxavier <professorxavier@users.noreply.github.com>
2018-10-15 12:41:33 -0300

    acerto no nome da base

--- clientserver/examples/products_catalog/base_reduzida.sql
+++ clientserver/examples/products_catalog/base_reduzida.sql
@@ -1,7 +1,6 @@
-
-create database aula3_clientserver;
-
-use aula3_clientserver;
+drop database if exists erp;
+create database erp;
+use erp;
 
 create table fabricante (
 
@@ -13,12 +12,12 @@ create table fabricante (
 
 create table produto (
 
-	produto_id int,
+	id int,
     nome varchar(200) NOT NULL,
     estoque int DEFAULT 0,
     preco float DEFAULT 0,
     cod_fabricante int,
-	constraint produto_PK primary key (produto_id),
+	constraint produto_PK primary key (id),
     constraint fabricante_FK foreign key (cod_fabricante) 
     references fabricante(fabricante_id) ON delete cascade
 );
@@ -26,6 +25,6 @@ create table produto (
 insert into fabricante (fabricante_id, nome) VALUES (1, 'Microsoft');
 insert into fabricante (fabricante_id, nome) VALUES (2, 'Logitech');
 insert into fabricante (fabricante_id, nome) VALUES (3, 'Dell');
-INSERT INTO produto (produto_id, nome, estoque, preco, cod_fabricante) VALUES (1,'ZHLGTK',46,77.13,1);
-INSERT INTO produto (produto_id, nome, estoque, preco, cod_fabricante) VALUES (2,'GGHQYE',71,92.25,1);
-INSERT INTO produto (produto_id, nome, estoque, preco, cod_fabricante) VALUES (3,'FW5G0C',67,72.52,1);
+INSERT INTO produto (id, nome, estoque, preco, cod_fabricante) VALUES (1,'ZHLGTK',46,77.13,1);
+INSERT INTO produto (id, nome, estoque, preco, cod_fabricante) VALUES (2,'GGHQYE',71,92.25,1);
+INSERT INTO produto (id, nome, estoque, preco, cod_fabricante) VALUES (3,'FW5G0C',67,72.52,1);
